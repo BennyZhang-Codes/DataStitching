e61a895bc02c0ee76fdef4d08b5c338f5165c711
Benny <823573476@qq.com>
2024-03-17 20:31:54 +0800

    add plot_kspace method

--- src/plot/plot_kspace.jl
+++ src/plot/plot_kspace.jl
@@ -1,4 +1,4 @@
-import KomaMRI.KomaMRIPlots: plot_kspace
+import KomaMRI.KomaMRIPlots: plot_kspace, interp_map, theme_chooser, plot_koma
 
 """
     p = plot_kspace(seq::Sequence; width=nothing, height=nothing, darkmode=false)
@@ -34,7 +34,7 @@ function plot_kspace(
     seq = hoseq.SEQ
 	bgcolor, text_color, plot_bgcolor, grid_color, sep_color = theme_chooser(darkmode)
 	#Calculations of theoretical k-space
-	kspace, kspace_adc = get_kspace(seq; Δt=1) #sim_params["Δt"])
+	K_nominal, K_nominal_adc, K_skope, K_skope_adc = get_kspace(hoseq; Δt=1) #sim_params["Δt"])
 	t_adc = KomaMRIBase.get_adc_sampling_times(seq)
 	#Colormap
 	c_map = [[t, "hsv($(floor(Int,(1-t)*255)), 100, 50)"] for t=range(0,1;length=10)] # range(s,b,N) only works in Julia 1.7.3
@@ -50,8 +50,8 @@ function plot_kspace(
 	end
 	c2 = interp_map(c_map, c2_idx ./ maximum(c2_idx))
 	#Layout
-	mink = minimum(kspace_adc,dims=1)
-	maxk = maximum(kspace_adc,dims=1)
+	mink = minimum(K_nominal_adc,dims=1)
+	maxk = maximum(K_nominal_adc,dims=1)
 	dW = maximum(maxk .- mink, dims=2) * .3
 	mink .-= dW
 	maxk .+= dW
@@ -75,12 +75,17 @@ function plot_kspace(
         l.width = width
     end
 	#Plot
-	p = [scatter() for j=1:3]
-	p[1] = scatter3d(x=kspace[:,1],y=kspace[:,2],z=kspace[:,3],mode="lines",
-			line=attr(color=c),name="Trajectory",hoverinfo="skip")
-	p[2] = scatter3d(x=kspace_adc[:,1],y=kspace_adc[:,2],z=kspace_adc[:,3],text=round.(t_adc*1e3,digits=3),mode="markers",
-			line=attr(color=c2),marker=attr(size=2),name="ADC",hovertemplate="kx: %{x:.1f} m⁻¹<br>ky: %{y:.1f} m⁻¹<br>kz: %{z:.1f} m⁻¹<br><b>t_acq</b>: %{text} ms<extra></extra>")
-	p[3] = scatter3d(x=[0],y=[0],z=[0],name="k=0",marker=attr(symbol="cross",size=10,color="red"))
+	p = [scatter() for j=1:5]
+	p[1] = scatter3d(x=K_nominal[:,1],y=K_nominal[:,2],z=K_nominal[:,3],mode="lines",
+			line=attr(color=c),name="nominal Traj",hoverinfo="skip")
+	p[2] = scatter3d(x=K_nominal_adc[:,1],y=K_nominal_adc[:,2],z=K_nominal_adc[:,3],text=round.(t_adc*1e3,digits=3),mode="markers",
+			line=attr(color=c2),marker=attr(size=2),name="nominal ADC",hovertemplate="nominal<br>kx: %{x:.1f} m⁻¹<br>ky: %{y:.1f} m⁻¹<br>kz: %{z:.1f} m⁻¹<br><b>t_acq</b>: %{text} ms<extra></extra>")
+	p[3] = scatter3d(x=K_skope[:,1],y=K_skope[:,2],z=K_skope[:,3],mode="lines",
+			line=attr(color=c),name="measured Traj",hoverinfo="skip")
+	p[4] = scatter3d(x=K_skope_adc[:,1],y=K_skope_adc[:,2],z=K_skope_adc[:,3],text=round.(t_adc*1e3,digits=3),mode="markers",
+			line=attr(color=c2),marker=attr(size=2),name="measured ADC",hovertemplate="measured<br>kx: %{x:.1f} m⁻¹<br>ky: %{y:.1f} m⁻¹<br>kz: %{z:.1f} m⁻¹<br><b>t_acq</b>: %{text} ms<extra></extra>")
+	
+	p[5] = scatter3d(x=[0],y=[0],z=[0],name="k=0",marker=attr(symbol="cross",size=10,color="red"))
 	config = PlotConfig(
 		displaylogo=false,
 		toImageButtonOptions=attr(
