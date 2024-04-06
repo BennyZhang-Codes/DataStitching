d995759b713ea9d9be96ec205123386b5afb1f5c
Benny <823573476@qq.com>
2024-04-06 17:09:34 +0800

    update simulate

--- src/simulation/Simulate.jl
+++ src/simulation/Simulate.jl
@@ -1,13 +1,13 @@
 import KomaMRI.KomaMRICore: simulate
 """
-    out = simulate(obj::Phantom, seq::HO_Sequence, sys::Scanner; sim_params, w)
+    out = simulate(obj::Phantom, hoseq::HO_Sequence, sys::Scanner; sim_params, w)
 
 Returns the raw signal or the last state of the magnetization according to the value
 of the `"return_type"` key of the `sim_params` dictionary.
 
 # Arguments
 - `obj`: (`::Phantom`) Phantom struct
-- `seq`: (`::Sequence`) Sequence struct
+- `hoseq`: (`::HO_Sequence`) HO_Sequence struct
 - `sys`: (`::Scanner`) Scanner struct
 
 # Keywords
@@ -19,17 +19,6 @@ of the `"return_type"` key of the `sim_params` dictionary.
 # Returns
 - `out`: (`::Vector{Complex}` or `::SpinStateRepresentation` or `::RawAcquisitionData`) depending
     on whether "return_type" is "mat", "state" or "raw" (default), respectively
-
-# Examples
-```julia-repl
-julia> seq_file = joinpath(dirname(pathof(KomaMRI)), "../examples/3.koma_paper/comparison_accuracy/sequences/EPI/epi_100x100_TE100_FOV230.seq");
-
-julia> sys, obj, seq = Scanner(), brain_phantom2D(), read_seq(seq_file)
-
-julia> raw = simulate(obj, hoseq, sys)
-
-julia> plot_signal(raw)
-```
 """
 function simulate(
     obj::Phantom, hoseq::HO_Sequence, sys::Scanner;
