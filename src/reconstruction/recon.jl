828ff96d18e49795f47d706e1fc5a6efbe090501
Benny <823573476@qq.com>
2024-04-06 15:04:17 +0800

    add reconstruction

--- src/reconstruction/recon.jl
+++ src/reconstruction/recon.jl
@@ -1,3 +1,4 @@
 include("SignalOp.jl")
 include("HighOrderOp.jl")
 include("recon_2d.jl")
+include("reconstrution.jl")
---
+++ src/reconstruction/reconstrution.jl
@@ -0,0 +1,6 @@
+import MRIReco: reconstruction
+
+function reconstruction(acqData::AcquisitionData, recoParams::Dict, method::BlochHighOrder)
+    @info "Reconstructing with $method"
+    # TODO: implement reconstruction
+end
