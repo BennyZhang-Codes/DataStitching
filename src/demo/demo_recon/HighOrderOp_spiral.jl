8c4878ec5921b19ddbf4d732f635db6c12fbbeb3
Benny <823573476@qq.com>
2024-04-08 20:23:17 +0800

    update demo

--- src/demo/demo_recon/HighOrderOp_spiral.jl
+++ src/demo/demo_recon/HighOrderOp_spiral.jl
@@ -1,9 +1,9 @@
 # julia -t 4
-using CUDA
-device!(1) 
+# using CUDA
+# device!(1) 
 
-using MRIReco, KomaHighOrder, MRICoilSensitivities, ProgressMeter
-
+using MRIReco, KomaHighOrder, MRICoilSensitivities, PlotlyJS
+dir = @__DIR__
 BHO_simu = "000"
 raw = demo_raw(BHO_simu)
 Nx, Ny = raw.params["reconSize"][1:2];
@@ -17,98 +17,21 @@ _, K_nominal_adc, _, K_skope_adc = get_kspace(hoseq; Δt=1)
 tr_skope = Trajectory(K_skope_adc'[:,:], acqData.traj[1].numProfiles, acqData.traj[1].numSamplingPerProfile, circular=false);
 tr_nominal = Trajectory(K_nominal_adc'[1:3,:], acqData.traj[1].numProfiles, acqData.traj[1].numSamplingPerProfile, circular=false);
 
-recParams = Dict{Symbol,Any}()
-recParams[:reconSize] = (Nx, Ny)  # 150, 150
-recParams[:densityWeighting] = true
-recParams[:reco] = "standard"
-recParams[:regularization] = "L2"
-recParams[:λ] = 1.e-2
-recParams[:iterations] = 3
-recParams[:solver] = "cgnr"
-
-BHO_reco = "000"
-Op = HighOrderOp(shape, tr_nominal, tr_skope, BlochHighOrder(BHO_reco);  Nblocks=3)
-recParams[:encodingOps] = reshape([Op], 1,1)
-@time rec = reconstruction(acqData, recParams);
-plot_image(abs.(rec.data[:,:]); title="Simu-$(BHO_simu)_Reco-$(BHO_reco)_CG-HighOrderOp", width=650, height=600)
-
-Op = SignalOp(shape, acqData.traj[1]; Nblocks=3)
-recParams[:encodingOps] = reshape([Op], 1,1)
-@time rec = reconstruction(acqData, recParams);
-plot_image(abs.(rec.data[:,:]); title="Sim111_CG-SignalOp", width=650, height=600)
-
-
-
-#######################################################################################
-# direct NUFFT
-#######################################################################################
-recParams = Dict{Symbol,Any}()
-recParams[:reconSize] = (Nx, Ny)
-recParams[:densityWeighting] = true
-@time rec = reconstruction(acqData, recParams);
-plot_image(abs.(rec.data[:,:]); title="direct NUFFT", width=650, height=600)
-
-#######################################################################################
-# iterative NUFFTOp
-#######################################################################################
-recParams = Dict{Symbol,Any}()
-recParams[:reconSize] = (Nx, Ny)  # 150, 150
-recParams[:densityWeighting] = true
-recParams[:reco] = "standard"
-recParams[:regularization] = "L2"
-recParams[:λ] = 1.e-2
-recParams[:iterations] = 20
-recParams[:solver] = "cgnr"
-@time rec = reconstruction(acqData, recParams);
-plot_image(abs.(rec.data[:,:]); title="iterative NUFFTOp", width=650, height=600)
-img_iter_NUFFTOp = abs.(rec.data[:,:])
-
-#######################################################################################
-# iterative SignalOp
-#######################################################################################
-recParams = Dict{Symbol,Any}()
-recParams[:reconSize] = (Nx, Ny)  # 150, 150
-recParams[:densityWeighting] = true
-recParams[:reco] = "standard"
-recParams[:regularization] = "L2"
-recParams[:λ] = 1.e-2
-recParams[:iterations] = 20
-recParams[:solver] = "cgnr"
-Op = SignalOp(shape, acqData.traj[1]; Nblocks=3)
-recParams[:encodingOps] = reshape([Op], 1,1)
-@time rec = reconstruction(acqData, recParams);
-plot_image(abs.(rec.data[:,:]); title="iterative SignalOp", width=650, height=600)
-img_iter_SignalOp = abs.(rec.data[:,:])
-
-plot_image(img_iter_NUFFTOp - img_iter_SignalOp; title="iter_NUFFTOp - iter_SignalOp", width=650, height=600)
-
-#######################################################################################
-# iterative HighOrderOp
-#######################################################################################
-recParams = Dict{Symbol,Any}()
-recParams[:reconSize] = (Nx, Ny)  # 150, 150
-recParams[:densityWeighting] = true
-recParams[:reco] = "standard"
-recParams[:regularization] = "L2"
-recParams[:λ] = 1.e-2
-recParams[:iterations] = 20
-recParams[:solver] = "cgnr"
-BHO_reco = "000"
-Op = HighOrderOp(shape, tr_nominal, tr_skope, BlochHighOrder(BHO_reco);  Nblocks=3)
-recParams[:encodingOps] = reshape([Op], 1,1)
-@time rec = reconstruction(acqData, recParams);
-plot_image(abs.(rec.data[:,:]); title="iterative HighOrderOp", width=650, height=600)
-img_iter_HighOrderOp = abs.(rec.data[:,:])
-
-plot_image(img_iter_HighOrderOp - img_iter_SignalOp; title="iter_HighOrderOp - iter_SignalOp", width=650, height=600)
-
 #######################################################################################
 # HighOrderOp 
 # [1] Simu: 111, Reco: 000
 # [2] Simu: 111, Reco: 100
-# [3] Simu: 111, Reco: 110
-# [4] Simu: 111, Reco: 111
+# [3] Simu: 111, Reco: 010
+# [4] Simu: 111, Reco: 001
+# [5] Simu: 111, Reco: 011
+# [6] Simu: 111, Reco: 101
+# [7] Simu: 111, Reco: 110
+# [8] Simu: 111, Reco: 111
 #######################################################################################
+BHO_simu = "111";raw = demo_raw(BHO_simu);
+Nx, Ny = raw.params["reconSize"][1:2];
+acqData = AcquisitionData(raw);acqData.traj[1].circular = false;shape = (Nx, Ny);
+
 recParams = Dict{Symbol,Any}()
 recParams[:reconSize] = (Nx, Ny)  # 150, 150
 recParams[:densityWeighting] = true
@@ -117,12 +40,119 @@ recParams[:regularization] = "L2"
 recParams[:λ] = 1.e-2
 recParams[:iterations] = 20
 recParams[:solver] = "cgnr"
-for BHO_reco in ["000", "100", "110", "111"]
-    Op = HighOrderOp(shape, tr_nominal, tr_skope, BlochHighOrder(BHO_reco);  Nblocks=3)
+BHO_recos = ["000", "100", "010", "001", "011", "101", "110", "111"]
+imgs = Array{ComplexF32,3}(undef, Nx, Ny, length(BHO_recos))
+for idx in eachindex(BHO_recos)
+    @info "Simu: $(BHO_simu), Reco: $(BHO_recos[idx])"
+    BHO_reco = BHO_recos[idx]
+    Op = HighOrderOp(shape, tr_nominal, tr_skope, BlochHighOrder(BHO_reco);  Nblocks=9)
     recParams[:encodingOps] = reshape([Op], 1,1)
     @time rec = reconstruction(acqData, recParams);
-    plot_image(abs.(rec.data[:,:]); title="Simu-$(BHO_simu)_Reco-$(BHO_reco)_CG-HighOrderOp", width=650, height=600)
+    imgs[:,:, idx] = rec.data[:,:]
 end
+
+imgs_111 = abs.(imgs)
+imgs_111_error = Array{Float32,3}(undef, size(imgs_111))
+for idx in eachindex(BHO_recos)
+    imgs_111_error[:,:, idx] = imgs_111[:,:, idx] - imgs_111[:,:, end]
+end
+
+subplot_titles = ["Reco: $t" for t in BHO_recos]
+title="HighOrderOp, Simu: $(BHO_simu)"
+
+p_111       = plot_imgs(imgs_111, subplot_titles; title=title, width=1300, height=200)
+p_111_error = plot_imgs(imgs_111_error, subplot_titles; title=title*", error map", width=1300, height=200)
+
+savefig(p_111,       dir*"/HighOrderOp_Simu_111.svg", width=1300, height=200,format="svg")
+savefig(p_111_error, dir*"/HighOrderOp_Simu_111_errormap.svg", width=1300, height=200,format="svg")
+
+#######################################################################################
+# HighOrderOp 
+# [1] Simu: 000, Reco: 000
+# [2] Simu: 000, Reco: 100
+# [3] Simu: 000, Reco: 010
+# [4] Simu: 000, Reco: 001
+# [5] Simu: 000, Reco: 011
+# [6] Simu: 000, Reco: 101
+# [7] Simu: 000, Reco: 110
+# [8] Simu: 000, Reco: 111
+#######################################################################################
+BHO_simu = "000";raw = demo_raw(BHO_simu);
+Nx, Ny = raw.params["reconSize"][1:2];
+acqData = AcquisitionData(raw);acqData.traj[1].circular = false;shape = (Nx, Ny);
+
+recParams = Dict{Symbol,Any}()
+recParams[:reconSize] = (Nx, Ny)  # 150, 150
+recParams[:densityWeighting] = true
+recParams[:reco] = "standard"
+recParams[:regularization] = "L2"
+recParams[:λ] = 1.e-2
+recParams[:iterations] = 20
+recParams[:solver] = "cgnr"
+BHO_recos = ["000", "100", "010", "001", "011", "101", "110", "111"]
+imgs = Array{ComplexF32,3}(undef, Nx, Ny, length(BHO_recos))
+for idx in eachindex(BHO_recos)
+    @info "Simu: $(BHO_simu), Reco: $(BHO_recos[idx])"
+    BHO_reco = BHO_recos[idx]
+    Op = HighOrderOp(shape, tr_nominal, tr_skope, BlochHighOrder(BHO_reco);  Nblocks=9)
+    recParams[:encodingOps] = reshape([Op], 1,1)
+    @time rec = reconstruction(acqData, recParams);
+    imgs[:,:, idx] = rec.data[:,:]
+end
+
+imgs_000 = abs.(imgs)
+imgs_000_error = Array{Float32,3}(undef, size(imgs_000))
+for idx in eachindex(BHO_recos)
+    imgs_000_error[:,:, idx] = imgs_000[:,:, idx] - imgs_000[:,:, 1]
+end
+
+subplot_titles = ["Reco: $t" for t in BHO_recos]
+title="HighOrderOp, Simu: $(BHO_simu)"
+
+p_000       = plot_imgs(imgs_000, subplot_titles; title=title, width=1300, height=200)
+p_000_error = plot_imgs(imgs_000_error, subplot_titles; title=title*", error map", width=1300, height=200)
+
+savefig(p_000,       dir*"/HighOrderOp_Simu_000.svg", width=1300, height=200,format="svg")
+savefig(p_000_error, dir*"/HighOrderOp_Simu_000_errormap.svg", width=1300, height=200,format="svg")
+
+##########################################################################################################
+# plot_imgs
+##########################################################################################################
+function plot_imgs(imgs, subplot_titles; title="HighOrderOp", width=1200, height=330)
+    fig = make_subplots(rows=1,cols=length(subplot_titles));
+    [add_trace!(fig, heatmap(z=imgs[:,:,idx], transpose=false,coloraxis="coloraxis"); row=1, col=idx) for idx in eachindex(subplot_titles)]
+    l = Layout(;
+        paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)",
+        title=attr(text=title, y=1, x=0.5, xanchor= "center", yanchor= "top", yref="container"),
+        margin=attr(t=40,l=0,r=100,b=0),
+        coloraxis=attr(colorscale="Greys",xanchor="left",colorbar=attr(x=1,thickness=20)),
+        font=attr(family="Times New Roman",size=16,color="gray"),
+    )
+    if height !== nothing
+        l.height = height
+    end
+    if width !== nothing
+        l.width = width
+    end
+    for idx in eachindex(subplot_titles)
+        Δdomain = 1/length(subplot_titles)
+        spacing = Δdomain/100
+        if idx == 1
+            push!(l.fields, Symbol("xaxis")=>attr(domain=(Δdomain*(idx-1)+spacing, Δdomain*idx-spacing),showticklabels=false,showgrid=false,zerolinecolor="rgba(0,0,0,0)"))
+            push!(l.fields, Symbol("yaxis")=>attr(scaleanchor="x", anchor="x", showticklabels=false,showgrid=false,zerolinecolor="rgba(0,0,0,0)"))
+        else 
+            push!(l.fields, Symbol("xaxis$idx")=>attr(domain=(Δdomain*(idx-1)+spacing, Δdomain*idx-spacing),showticklabels=false,showgrid=false,zerolinecolor="rgba(0,0,0,0)"))
+            push!(l.fields, Symbol("yaxis$idx")=>attr(scaleanchor="x$idx", anchor="x$idx", showticklabels=false,showgrid=false,zerolinecolor="rgba(0,0,0,0)"))
+        end
+    end
+    annotations=[attr(text=subplot_titles[idx],yanchor="bottom",xanchor="center",xref="x$idx domain",x=0.5,yref="y$idx domain",y=1,showarrow=false,font=attr(size=16)) for idx in eachindex(subplot_titles)]
+    push!(l.fields, :annotations=>annotations)
+    p = update(fig; layout=l)
+    return p
+end
+
+
+
 
 
 
--- src/demo/demo_recon/SignalOp_spiral.jl
+++ src/demo/demo_recon/SignalOp_spiral.jl
@@ -1,34 +1,151 @@
 # julia -t 4
-device!(1)
-name.(devices())
+# using CUDA
+# device!(1) 
 
-ENV["OMP_NUM_THREADS"] = 4
-using MRIReco, KomaHighOrder, MRICoilSensitivities, ProgressMeter
-
-raw = demo_raw("000")
-
+using MRIReco, KomaHighOrder, MRICoilSensitivities, PlotlyJS
+dir = @__DIR__
+BHO_simu = "000"
+raw = demo_raw(BHO_simu)
 Nx, Ny = raw.params["reconSize"][1:2];
 acqData = AcquisitionData(raw);
-acqData.traj[1].circular = false
-shape = (Nx, Ny)
+acqData.traj[1].circular = false;
+shape = (Nx, Ny);
 
-HOOp = SignalOp(shape, acqData.traj[1]; Nblocks=3)
+hoseq = demo_hoseq()
+_, K_nominal_adc, _, K_skope_adc = get_kspace(hoseq; Δt=1)
 
-T = Float32
+tr_skope = Trajectory(K_skope_adc'[:,:], acqData.traj[1].numProfiles, acqData.traj[1].numSamplingPerProfile, circular=false);
+tr_nominal = Trajectory(K_nominal_adc'[1:3,:], acqData.traj[1].numProfiles, acqData.traj[1].numSamplingPerProfile, circular=false);
+
+#######################################################################################
+# direct NUFFT
+#######################################################################################
+# acqData.traj[1] = Trajectory(Float32.(K_nominal_adc'[1:3,:]), acqData.traj[1].numProfiles, acqData.traj[1].numSamplingPerProfile, circular=false);
+
+recParams = Dict{Symbol,Any}()
+recParams[:reconSize] = (Nx, Ny)
+recParams[:densityWeighting] = true
+@time rec = reconstruction(acqData, recParams);
+# p_direct_NUFFTOp = plot_image(abs.(rec.data[:,:]); title="direct NUFFTOp", width=650, height=600)
+img_direct_NUFFTOp = abs.(rec.data[:,:]);
+# savefig(p_direct_NUFFTOp,       dir*"/direct_NUFFTOp.svg", width=650, height=600,format="svg")
+#######################################################################################
+# iterative NUFFTOp
+#######################################################################################
 recParams = Dict{Symbol,Any}()
 recParams[:reconSize] = (Nx, Ny)  # 150, 150
 recParams[:densityWeighting] = true
 recParams[:reco] = "standard"
 recParams[:regularization] = "L2"
-recParams[:λ] = T(1.e-2)
-recParams[:iterations] = 1
+recParams[:λ] = 1.e-2
+recParams[:iterations] = 20
 recParams[:solver] = "cgnr"
-recParams[:encodingOps] = reshape([HOOp], 1,1)
-
 @time rec = reconstruction(acqData, recParams);
-image3d  = reshape(rec.data, Nx, Ny, :);
-image2d = (abs.(image3d) * prod(size(image3d)[1:2]))[:,:,1];
-plot_image(image2d; title="iterative_CG-SignalOp", width=650, height=600)
+# p_iter_NUFFTOp = plot_image(abs.(rec.data[:,:]); title="iterative NUFFTOp", width=650, height=600)
+img_iter_NUFFTOp = abs.(rec.data[:,:]);
+
+#######################################################################################
+# iterative SignalOp normalized
+#######################################################################################
+recParams = Dict{Symbol,Any}()
+recParams[:reconSize] = (Nx, Ny)  # 150, 150
+recParams[:densityWeighting] = true
+recParams[:reco] = "standard"
+recParams[:regularization] = "L2"
+recParams[:λ] = 1.e-2
+recParams[:iterations] = 20
+recParams[:solver] = "cgnr"
+
+Op = SignalOp(shape, acqData.traj[1]; Nblocks=9)
+recParams[:encodingOps] = reshape([Op], 1,1)
+@time rec = reconstruction(acqData, recParams);
+p_iter_SignalOp_normalized = plot_image(abs.(rec.data[:,:]); title="iterative SignalOp normalized", width=650, height=600)
+img_iter_SignalOp_normalized = abs.(rec.data[:,:]);
+
+#######################################################################################
+# iterative SignalOp 
+#######################################################################################
+recParams = Dict{Symbol,Any}()
+recParams[:reconSize] = (Nx, Ny)  # 150, 150
+recParams[:densityWeighting] = true
+recParams[:reco] = "standard"
+recParams[:regularization] = "L2"
+recParams[:λ] = 1.e-2
+recParams[:iterations] = 20
+recParams[:solver] = "cgnr"
+
+Op = SignalOp(shape, tr_nominal, 1e-3, 1e-3; Nblocks=9)
+recParams[:encodingOps] = reshape([Op], 1,1)
+@time rec = reconstruction(acqData, recParams);
+p_iter_SignalOp = plot_image(abs.(rec.data[:,:]); title="iterative SignalOp", width=650, height=600)
+img_iter_SignalOp = abs.(rec.data[:,:]);
+
+plot_image(img_iter_SignalOp - img_iter_SignalOp_normalized; title="iter_SignalOp - iter_SignalOp_normalized", width=650, height=600)
+
+#######################################################################################
+# iterative HighOrderOp
+#######################################################################################
+recParams = Dict{Symbol,Any}()
+recParams[:reconSize] = (Nx, Ny)  # 150, 150
+recParams[:densityWeighting] = true
+recParams[:reco] = "standard"
+recParams[:regularization] = "L2"
+recParams[:λ] = 1.e-2
+recParams[:iterations] = 20
+recParams[:solver] = "cgnr"
+BHO_reco = "000"
+Op = HighOrderOp(shape, tr_nominal, tr_skope, BlochHighOrder(BHO_reco);  Nblocks=9)
+recParams[:encodingOps] = reshape([Op], 1,1)
+@time rec = reconstruction(acqData, recParams);
+# p_iter_HighOrderOp = plot_image(abs.(rec.data[:,:]); title="iterative HighOrderOp", width=650, height=600)
+img_iter_HighOrderOp = abs.(rec.data[:,:])
 
 
 
+
+
+##### plots
+imgs = Array{Float32,3}(undef, Nx, Ny, 5)
+imgs[:,:, 1] = img_direct_NUFFTOp; imgs[:,:, 2] = img_iter_NUFFTOp; imgs[:,:, 3] = img_iter_SignalOp_normalized; imgs[:,:, 4] = img_iter_SignalOp; imgs[:,:, 5] = img_iter_HighOrderOp
+p_imgs = plot_imgs(imgs, ["direct NUFFTOp", "NUFFTOp", "SignalOp NormKspace", "SignalOp", "HighOrderOp"]; title="", width=1100, height=250)
+savefig(p_imgs, dir*"/SignalOp_compare_nominalrecon.svg", width=1100, height=250,format="svg")
+
+
+imgs_errors = Array{Float32,3}(undef, Nx, Ny, 3)
+imgs_errors[:,:, 1] = img_iter_SignalOp - img_iter_NUFFTOp; 
+imgs_errors[:,:, 2] = img_iter_HighOrderOp - img_iter_NUFFTOp; 
+imgs_errors[:,:, 3] = img_iter_HighOrderOp - img_iter_SignalOp;
+p_imgs_errors = plot_imgs(imgs_errors, ["SignalOp - NUFFTOp", "HighOrderOp - NUFFTOp", "HighOrderOp - SignalOp"]; title="", width=780, height=250)
+savefig(p_imgs_errors, dir*"/SignalOp_compare_nominalrecon_errors.svg", width=780, height=250,format="svg")
+
+p = plot_imgs(img_iter_SignalOp_normalized - img_iter_NUFFTOp , ["SignalOp NormKspace - NUFFTOp",]    ; title="", width=350, height=250)
+savefig(p, dir*"/SignalOp_SignalOpNormKspace-NUFFTOp.svg", width=350, height=250,format="svg")
+
+plot_imgs(img_iter_SignalOp - img_iter_NUFFTOp , ["SignalOp - NUFFTOp",]    ; title="", width=500, height=500)
+plot_imgs(img_iter_HighOrderOp - img_iter_NUFFTOp , ["HighOrderOp - NUFFTOp",] ; title="", width=500, height=500)
+plot_imgs(img_iter_HighOrderOp - img_iter_SignalOp, ["HighOrderOp - SignalOp",]; title="", width=500, height=500)
+
+
+Δx = Δy = 1e-3 # m
+
+x, y = 1:Nx, 1:Ny
+x, y, z = vec(x .+ y'*0.0), vec(x*0.0 .+ y'), vec(x*0.0 .+ y'*0.0) #grid points
+x, y = x .- Nx/2 .- 1, y .- Ny/2 .- 1
+plot_bgcolor = "rgb(22,26,29)"
+grid_color = "rgb(40,52,66)"
+p1 = plot(scatter3d(x=x, y=y, z=z,       marker=attr(size=0.7), mode="markers"), Layout(
+    paper_bgcolor="rgba(0,0,0,0)", 
+    scene=attr(xaxis=attr(backgroundcolor=plot_bgcolor,gridcolor=grid_color,zerolinecolor=grid_color),
+                yaxis=attr(backgroundcolor=plot_bgcolor,gridcolor=grid_color,zerolinecolor=grid_color),
+                zaxis=attr(backgroundcolor=plot_bgcolor,gridcolor=grid_color,zerolinecolor=grid_color)),
+    font=attr(family="Times New Roman",color="gray"),
+    width=500,height=500))
+p2 = plot(scatter3d(x=x*Δx, y=y*Δy, z=z, marker=attr(size=0.7), mode="markers"), Layout(
+    paper_bgcolor="rgba(0,0,0,0)",
+    scene=attr(xaxis=attr(backgroundcolor=plot_bgcolor,gridcolor=grid_color,zerolinecolor=grid_color),
+                yaxis=attr(backgroundcolor=plot_bgcolor,gridcolor=grid_color,zerolinecolor=grid_color),
+                zaxis=attr(backgroundcolor=plot_bgcolor,gridcolor=grid_color,zerolinecolor=grid_color)),
+    font=attr(family="Times New Roman",color="gray"),
+    width=500,height=500))
+savefig(p1, dir*"/grid_points_index.svg",            width=500,height=500,format="svg")
+savefig(p2, dir*"/grid_points_physicalposition.svg", width=500,height=500,format="svg")
