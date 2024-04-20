028f83559605e5119af253d3a72d1151ffd13845
Benny <823573476@qq.com>
2024-04-20 16:18:34 +0800

    update phantom reference

--- src/demo/demo_recon/HighOrderOp_spiral/phantom_reference.jl
+++ src/demo/demo_recon/HighOrderOp_spiral/phantom_reference.jl
@@ -1,9 +1,4 @@
 using MAT, PlotlyJS, Statistics,ImageTransformations
-function get_center_range(x::Int64, x_range::Int64)
-    center = Int64(floor(x/2))
-    return center - Int64(ceil(x_range/2))+1 : center + 1 + Int64(ceil(x_range/2))-1
-end
-
 # Normalization
 function normalization(img::AbstractArray{T,2}) where T<:Real
     return (img.- minimum(img))./ (maximum(img) .- minimum(img))
@@ -11,26 +6,13 @@ end
 function standardization(img::AbstractArray{T,2}) where T<:Real
     return (img.- mean(img))./ std(img)
 end
-dir = "$(@__DIR__)/src/demo/demo_recon/HighOrderOp_spiral"
+dir = "$(@__DIR__)/src/demo/demo_recon/HighOrderOp_spiral/results"
 
-ρ1 = demo_obj(; axis="axial", ss=1, location=0.8)';
-M, N = size(ρ1)
-ρ1 = ρ1[get_center_range(M, 750), get_center_range(N, 750)]; # 1mm 
-plot_image(ρ1; title="0.2mm iso, ss=1")
-ρ1 = imresize(ρ1, (150, 150));
-plot_image(ρ1; title="1mm iso, ss=1")
-
-ρ2 = demo_obj(; axis="axial", ss=5, location=0.8)[get_center_range(217, 150), get_center_range(181, 150)]';
-plot_image(ρ2; title="1mm iso, ss=5")
-
-plot_image(ρ1 - ρ2; title="difference: ρ1 - ρ2", zmin=minimum(ρ1 - ρ2), zmax=maximum(ρ1 - ρ2))
-
-
-savefig(plot_image(ρ'; title="Phantom reference"),  dir*"/HighOrderOp_Simu_PhantomReference.svg", width=400, height=350,format="svg")
-
+ρ = demo_PhantomReference();
+p_ref = plot_image(ρ; title="PhantomReference[$(size(ρ)) | 1mm | ρ]")
+savefig(p_ref,  dir*"/HighOrderOp_Simu_PhantomReference.svg", width=400, height=350,format="svg")
 mat_111 = MAT.matread("$dir/HighOrderOp_Simu_111.mat")  # keys: ["imgs", "imgs_error", "BHO"]
 mat_000 = MAT.matread("$dir/HighOrderOp_Simu_000.mat")
-
 
 
 imgs_111    = mat_111["imgs"];
@@ -39,13 +21,10 @@ BHO_recos = mat_111["BHO"];
 subplot_titles = ["Reco: $t" for t in BHO_recos];
 title="HighOrderOp, Simu: 111";
 
-# p_111       = plot_imgs(imgs_111, subplot_titles; title=title, width=1300, height=200)
-# savefig(p_000,       dir*"/HighOrderOp_Simu_000.svg", width=1300, height=200,format="svg")
-
 imgs_error_111 = Array{Float32,3}(undef, size(imgs_111));
 imgs_normalized_111 = Array{Float32,3}(undef, size(imgs_111));
 for idx in eachindex(BHO_recos)
-    imgs_error_111[:,:, idx] = ρ2 - normalization(imgs_111[:,:, idx]);
+    imgs_error_111[:,:, idx] = ρ - normalization(imgs_111[:,:, idx]);
     imgs_normalized_111[:,:, idx] = normalization(imgs_111[:,:, idx]);
 end
 
--- src/demo/demo_recon/SignalOp_spiral/phantom_reference.jl
+++ src/demo/demo_recon/SignalOp_spiral/phantom_reference.jl
@@ -1,8 +1,4 @@
 using MAT, PlotlyJS, Statistics,ImageTransformations
-function get_center_range(x::Int64, x_range::Int64)
-    center = Int64(floor(x/2))
-    return center - Int64(ceil(x_range/2))+1 : center + 1 + Int64(ceil(x_range/2))-1
-end
 
 # Normalization
 function normalization(img::AbstractArray{T,2}) where T<:Real
@@ -11,44 +7,12 @@ end
 function standardization(img::AbstractArray{T,2}) where T<:Real
     return (img.- mean(img))./ std(img)
 end
-dir = "$(@__DIR__)/src/demo/demo_recon/SignalOp_spiral"
 
-Δx = Δy = 0.2
+dir = "$(@__DIR__)/src/demo/demo_recon/SignalOp_spiral/results"
 
-ss=3
-location=0.8
-fov=(150, 150)
-resolution=(1,1)
-
-fov_x, fov_y = fov
-res_x, res_y = resolution
-
-M, N = size(a)
-a = a[get_center_range(M, Int64(ceil(fov_x / (Δx * ss)))), get_center_range(N, Int64(ceil(fov_y / (Δy * ss))))]
-a = imresize(a, (Int64(ceil(fov_x / res_x)), Int64(ceil(fov_y / res_y))))
-plot_image(a')
-
-ρ1 = demo_obj(; axis="axial", ss=1, location=0.8)';
-M, N = size(ρ1)
-ρ1 = ρ1[get_center_range(M, 750), get_center_range(N, 750)]; # 1mm 
-plot_image(ρ1; title="0.2mm iso, ss=1")
-ρ1 = imresize(ρ1, (150, 150));
-plot_image(ρ1; title="1mm iso, ss=1")
-
-ρ2 = demo_obj(; axis="axial", ss=5, location=0.8)[get_center_range(217, 150), get_center_range(181, 150)]';
-plot_image(ρ2; title="1mm iso, ss=5")
-
-ρ3 = demo_obj(; axis="axial", ss=3, location=0.8)[get_center_range(362, 250), get_center_range(302, 250)]';
-plot_image(ρ3; title="1mm iso, ss=3")
-ρ3 = imresize(ρ3, (150, 150));
-
-
-plot_image(ρ3 - ρ2; title="difference: ρ3 - ρ2", zmin=minimum(ρ3 - ρ2), zmax=maximum(ρ3 - ρ2))
-plot_image(ρ3 - ρ1; title="difference: ρ3 - ρ1", zmin=minimum(ρ3 - ρ1), zmax=maximum(ρ3 - ρ1))
-plot_image(ρ1 - ρ2; title="difference: ρ1 - ρ2", zmin=minimum(ρ1 - ρ2), zmax=maximum(ρ1 - ρ2))
-
-
-savefig(plot_image(ρ'; title="Phantom reference"),  dir*"/HighOrderOp_Simu_PhantomReference.svg", width=400, height=350,format="svg")
+ρ = demo_PhantomReference();
+p_ref = plot_image(ρ; title="PhantomReference[$(size(ρ)) | 1mm | ρ]")
+savefig(p_ref,  dir*"/HighOrderOp_Simu_PhantomReference.svg", width=400, height=350,format="svg")
 
 mat = MAT.matread("$dir/SignalOp_Simu_000.mat");  # keys: ["img_direct_NUFFTOp","img_iter_NUFFTOp","img_iter_SignalOp_normalized","img_iter_SignalOp","img_iter_HighOrderOp"]
 img_direct_NUFFTOp = mat["img_direct_NUFFTOp"];
@@ -64,11 +28,10 @@ p_imgs = plot_imgs(imgs, ["direct NUFFTOp", "NUFFTOp", "SignalOp NormKspace", "S
 
 title="Simu: 000";
 
-
 imgs_error = Array{Float32,3}(undef, size(imgs));
 imgs_normalized = Array{Float32,3}(undef, size(imgs));
 for idx = 1:5
-    imgs_error[:,:, idx] = ρ3 - normalization(imgs[:,:, idx]);
+    imgs_error[:,:, idx] = ρ - normalization(imgs[:,:, idx]);
     imgs_normalized[:,:, idx] = normalization(imgs[:,:, idx]);
 end
 
