6811b51639d24887e11e599c71f6dd1155585dd3
Benny <823573476@qq.com>
2024-04-22 16:12:47 +0800

    add demo_cgsense

---
+++ src/demo/demo_sense/1.sim.jl
@@ -0,0 +1,40 @@
+using KomaHighOrder
+
+
+Ncoils = 3
+BHO_name = "000"
+
+
+folder = "Ncoils$Ncoils"
+path = "$(@__DIR__)/src/demo/demo_sense/$folder"
+if ispath(path) == false mkdir(path) end
+
+hoseq = demo_hoseq()
+
+sys = Scanner();
+sim_params = KomaMRICore.default_sim_params(); 
+sim_params["sim_method"] = BlochHighOrder(BHO_name);
+sim_params["gpu"] = true;
+sim_params["return_type"]="mat";
+
+
+signal = zeros(ComplexF64, sum(hoseq.SEQ.ADC.N), Ncoils);
+for coil_idx = 1:Ncoils
+    obj = brain_phantom2D(brain2D(), coil_idx, Ncoils; ss=1, location=0.8); 
+    obj.Δw .= obj.Δw * 0; # γ*1.5*(-3.45)*1e-6 * 2π
+    obj.T2 .= obj.T2 * Inf; 
+    # simulate
+    signal[:, coil_idx] = simulate(obj, hoseq, sys; sim_params);
+    # protocolName = "$(hoseq.SEQ.DEF["Name"])_$(BHO_name)_nominal_$(coil_idx)-$(Ncoils)"
+    # p = plot_image(reconstruct_2d_image(signal_to_raw_data(signal[:, coil_idx:coil_idx], hoseq, :nominal)); 
+    #     title="$(sim_params["sim_method"]) Nominal $(coil_idx)-$(Ncoils)", height=700, width=750)
+    # savefig(p,  "$(path)/$(protocolName).svg",format="svg", height=700, width=750)
+end
+
+raw = signal_to_raw_data(signal, hoseq, :nominal)
+filename = "$(hoseq.SEQ.DEF["Name"])_$(BHO_name)_nominal_Ncoils$(Ncoils)"
+raw.params["protocolName"] = filename
+mrd = ISMRMRDFile("$(path)/$(filename).mrd")
+save(mrd, raw)
+
+
---
+++ src/demo/demo_sense/2.recon.jl
@@ -0,0 +1,58 @@
+using KomaHighOrder
+using MRIReco, MRICoilSensitivities, MRISampling
+
+Ncoils = 3
+BHO_name = "000"
+hoseq = demo_hoseq()
+filename = "$(hoseq.SEQ.DEF["Name"])_$(BHO_name)_nominal_Ncoils$(Ncoils)"
+
+folder = "Ncoils$Ncoils"
+path = "$(@__DIR__)/src/demo/demo_sense/$folder"
+
+raw_file = "$(path)/$(filename).mrd"
+@assert ispath(raw_file) "the raw file does not exist: $(raw_file)"
+raw = RawAcquisitionData(ISMRMRDFile(raw_file));
+Nx, Ny = raw.params["reconSize"][1:2];
+acqData = AcquisitionData(raw);
+acqData.traj[1].circular = false;
+shape = (Nx, Ny);
+
+# espirit
+# acqDataCart = regrid(acqData, (Nx, Ny); cgnr_iter=3);
+# sensitivity = espirit(acqDataCart, (6,6), 30, eigThresh_1=0.02, eigThresh_2=0.98);
+
+# fan mask
+mask = get_fan_mask(Nx, Ny, Ncoils);
+plot_image(mask')
+sensitivity = Array{ComplexF32,4}(undef, Nx, Ny, 1, Ncoils);
+for c = 1:Ncoils
+    sensitivity[:,:,1,c] = ((mask .== c) .* 1)'
+end
+
+
+
+# s = [sensitivity[:,:,1,i] for i=1:Ncoils];
+# s = [s[1] s[2] s[3]];
+# plot_image(abs.(rotr90(s)); title="sensitivity - espirit")
+# plot_image(abs.(((mask .== 1) .* 1)'))
+
+@info "reference reco"
+T = Float32;
+params = Dict{Symbol, Any}()
+params[:reco] = "multiCoil"
+params[:reconSize] = (Nx, Ny)
+params[:regularization] = "L2"
+params[:λ] = T(1.e-2)
+params[:iterations] = 1
+params[:relTol] = 0.0
+params[:solver] = "cgnr"
+params[:toeplitz] = false
+params[:oversamplingFactor] = 2
+params[:senseMaps] = Complex{T}.(reshape(sensitivity, Nx, Ny, 1, Ncoils));
+
+img_cg = Array{ComplexF32,2}(undef, Nx, Ny);
+
+img_cg = reconstruction(acqData, params).data;
+
+plot_image(abs.(img_cg[:,:]))
+
