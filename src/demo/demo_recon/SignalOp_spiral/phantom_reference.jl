f0385f92ef5a5ad4db84706f7068755123b14c40
Benny <823573476@qq.com>
2024-04-24 13:08:30 +0800

    update results

--- src/demo/demo_recon/SignalOp_spiral/phantom_reference.jl
+++ src/demo/demo_recon/SignalOp_spiral/phantom_reference.jl
@@ -2,13 +2,16 @@ using MAT, PlotlyJS, Statistics
 using ImageTransformations, ImageQualityIndexes, ImageDistances
 
 
-folder = "woT2B0"   #  "woT2B0", "woB0_wT2"   
+folder = "woB0_wT2"   #  "woT2B0", "woB0_wT2"   
 dir = "$(@__DIR__)/src/demo/demo_recon/SignalOp_spiral/results/$folder"
 
 ρ = brain_phantom2D_reference(brain2D(); ss=3, location=0.8, key=:ρ, target_fov=(150, 150), target_resolution=(1,1));
-ρ = Matrix{Float32}(ρ);
-p_ref = plot_image(ρ; title="PhantomReference[$(size(ρ)) | 1mm | ρ]")
-savefig(p_ref,  dir*"/PhantomReference_ss3_location0.8.svg", width=400, height=350,format="svg")
+mask = brain_phantom2D_reference(brain2D(); ss=3, location=0.8, key=:binary, target_fov=(150, 150), target_resolution=(1,1));
+
+p_ref = plot_image(ρ; title="PhantomReference[ $(size(ρ)) | 1mm | ρ ]")
+savefig(p_ref,  dir*"/PhantomReference_ss3_location0.8_rho.svg", width=500, height=450,format="svg")
+p_ref_mask = plot_image(mask; title="PhantomReference[ $(size(mask)) | 1mm | binarymask ]")
+savefig(p_ref_mask,  dir*"/PhantomReference_ss3_location0.8_binary.svg", width=500, height=450,format="svg")
 
 mat = MAT.matread("$dir/SignalOp_Simu_000.mat");  # keys: ["img_direct_NUFFTOp","img_iter_NUFFTOp","img_iter_SignalOp_normalized","img_iter_SignalOp","img_iter_HighOrderOp"]
 img_direct_NUFFTOp = mat["img_direct_NUFFTOp"];
@@ -22,36 +25,60 @@ imgs[:,:, 1] = img_direct_NUFFTOp; imgs[:,:, 2] = img_iter_NUFFTOp; imgs[:,:, 3]
 p_imgs = plot_imgs(imgs, ["direct NUFFTOp", "NUFFTOp", "SignalOp NormKspace", "SignalOp", "HighOrderOp"]; title="", width=1100, height=250)
 
 
-# metrics 
-mse_values = Vector{Float32}(undef, size(imgs)[end])
-ssim_values = Vector{Float32}(undef, size(imgs)[end])
-for idx = 1:5
-    mse_values[idx] = mse(normalization(imgs[:,:, idx]), ρ)
-    ssim_values[idx] = assess_ssim(normalization(imgs[:,:, idx]), ρ)
-end
-
 # error map & imgs normalized to [0,1]
 imgs_error = Array{Float32,3}(undef, size(imgs));
 imgs_normalized = Array{Float32,3}(undef, size(imgs));
+imgs_error_mask = Array{Float32,3}(undef, size(imgs));
+imgs_normalized_mask = Array{Float32,3}(undef, size(imgs));
 for idx = 1:5
     imgs_error[:,:, idx] = ρ - normalization(imgs[:,:, idx]);
     imgs_normalized[:,:, idx] = normalization(imgs[:,:, idx]);
+    imgs_error_mask[:,:, idx]  = imgs_error[:,:, idx] .* mask;
+    imgs_normalized_mask[:,:, idx] = imgs_normalized[:,:, idx] .* mask;
 end
 
 
 title="Simu: 000, $folder";
 subplot_titles = ["direct NUFFTOp", "NUFFTOp", "SignalOp NormKspace", "SignalOp", "HighOrderOp"]
 width=1000; height=210;
-p_normalized = plot_imgs(imgs_normalized, subplot_titles; title=title*" | normalized to [0,1]", width=width, height=height)
-savefig(p_normalized,  dir*"/SignalOp_Simu_000_Normalized.svg",format="svg", width=width+100, height=height+40)
 
+# plot_imgs: error map
+mse_values = Vector{Float32}(undef, size(imgs)[end])
+ssim_values = Vector{Float32}(undef, size(imgs)[end])
+for idx = 1:5
+    mse_values[idx] = mse(normalization(imgs[:,:, idx]), ρ)
+    ssim_values[idx] = assess_ssim(normalization(imgs[:,:, idx]), ρ)
+end
 annotations = []
 for idx in eachindex(subplot_titles)
     push!(annotations, attr(text="SSIM: $(ssim_values[idx])<br>MSE: $(mse_values[idx])",
     yanchor="top",xanchor="center",xref="x$idx domain",x=0.5,yref="y$idx domain",y=0,showarrow=false,font=attr(size=14)))
 end
-p_error = plot_imgs(imgs_error, subplot_titles; title=title*" | error map", width=width, height=height, annotations=annotations, margin_bottom=40)
+p_error = plot_imgs(imgs_error, subplot_titles; title=title*" | error map", 
+                    width=width, height=height, annotations=annotations, margin_bottom=40)
 savefig(p_error,  dir*"/SignalOp_Simu_000_ErrorWithPhantom.svg",format="svg", width=width+100, height=height+40+40)
+
+# plot_imgs: error map masked
+mse_values = Vector{Float32}(undef, size(imgs)[end])
+ssim_values = Vector{Float32}(undef, size(imgs)[end])
+for idx = 1:5
+    mse_values[idx] = mse(normalization(imgs[:,:, idx])[mask.==1], ρ[mask.==1])
+    ssim_values[idx] = assess_ssim(normalization(imgs[:,:, idx]).*mask, ρ.*mask)
+end
+annotations = []
+for idx in eachindex(subplot_titles)
+    push!(annotations, attr(text="SSIM: $(ssim_values[idx])<br>MSE: $(mse_values[idx])",
+    yanchor="top",xanchor="center",xref="x$idx domain",x=0.5,yref="y$idx domain",y=0,showarrow=false,font=attr(size=14)))
+end
+p_error_mask = plot_imgs(imgs_error_mask, subplot_titles; title=title*" | error map masked", 
+                        width=width, height=height, annotations=annotations, margin_bottom=40)
+savefig(p_error_mask,  dir*"/SignalOp_Simu_000_ErrorWithPhantom_masked.svg",format="svg", width=width+100, height=height+40+40)
+
+# plot_imgs: imgs normalized to [0,1]
+p_normalized = plot_imgs(imgs_normalized, subplot_titles; title=title*" | normalized to [0,1]", width=width, height=height)
+p_normalized_mask = plot_imgs(imgs_normalized_mask, subplot_titles; title=title*" | normalized to [0,1], masked", width=width, height=height)
+savefig(p_normalized,  dir*"/SignalOp_Simu_000_Normalized.svg",format="svg", width=width+100, height=height+40)
+savefig(p_normalized_mask,  dir*"/SignalOp_Simu_000_Normalized_masked.svg",format="svg", width=width+100, height=height+40)
 
 
 # difference map: SignalOp - SignalOp_NormKspace
