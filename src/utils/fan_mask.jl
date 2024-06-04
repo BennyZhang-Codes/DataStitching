b4423825d13caafe44f2e0f892b41589eab76151
Benny <823573476@qq.com>
2024-06-04 17:08:29 +0800

    add overlap in get_fan_mask function

--- src/utils/fan_mask.jl
+++ src/utils/fan_mask.jl
@@ -1,6 +1,32 @@
 export get_fan_mask
 
-function get_fan_mask(Nx::Int64, Ny::Int64, Nparts::Int64)
+
+
+"""
+    mask = get_fan_mask(Nx::Int64, Ny::Int64, Nparts::Int64; overlap::Real=1)
+
+# Description
+    get fan-shaped mask with arbitrary overlap.
+
+# Arguments
+- `Nx`: (`::Int64`) 
+- `Ny`: (`::Int64`) 
+- `Nparts`: (`::Int64`) 
+
+# Keywords
+- `overlap`: (`::`) plot width
+
+
+# Returns
+- `mask`: (`::Array{Float64, 3}`) mask (Nx, Ny, Nparts)
+
+# Examples
+```julia-repl
+julia> mask = fan_mask(100, 100, 6; overlap=1.5)
+julia> plot_imgs_subplots(mask, 2, 3)
+```
+"""
+function get_fan_mask(Nx::Int64, Ny::Int64, Nparts::Int64; overlap::Real=1)
     @info "fan mask" Nx=Nx Ny=Ny Nparts=Nparts
     m_x = (1:1:Nx) .* ones(1, Ny)
     m_y = ones(Nx) .* (1:1:Ny)'
@@ -11,16 +37,15 @@ function get_fan_mask(Nx::Int64, Ny::Int64, Nparts::Int64)
     ϕ = reshape(angle.(Δx .+ Δy*im), (Nx, Ny))     # split cartesien grids by angle ϕ
     # plot_image(ϕ; zmin=-pi)
 
-    mask = zeros(Nx, Ny)
+    mask = zeros(Nx, Ny, Nparts)
     for i = 1:Nparts
         angle_rad = collect(-pi:2pi/Nparts:pi)[i]
-        mask[abs.(ϕ .- angle_rad) .>= (2pi - pi/Nparts) .|| abs.(ϕ .- angle_rad) .<= pi/Nparts] .= i
+        println("angle_rad = ", angle_rad)
+        m = mask[:,:,i]
+        m[abs.(ϕ .- angle_rad) .>= (2pi - pi/Nparts*overlap) .|| abs.(ϕ .- angle_rad) .<= pi/Nparts*overlap] .= 1
+        mask[:,:,i] = m
     end
     return mask
-    # plot_image(mask; zmin=minimum(mask), zmax=maximum(mask))
+    # plot_imgs_subplots(mask, 2,2)
 end
-
-
-
-
 
