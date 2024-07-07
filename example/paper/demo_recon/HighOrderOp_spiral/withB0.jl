4d6d0193ce7214cc70a302671ca1cc79a4194d8c
Benny <823573476@qq.com>
2024-07-07 17:06:58 +0800

    update

--- example/paper/demo_recon/HighOrderOp_spiral/withB0.jl
+++ example/paper/demo_recon/HighOrderOp_spiral/withB0.jl
@@ -114,30 +114,3 @@ p_iter_SignalOp = plot_image(abs.(rec.data[:,:]); title="HighOrderOp 111 with B0
 end
 
 
-["L2", "L1", "L21", "TV", "LLR", "Positive", "Proj", "Nuclear"]
-"LLR", "Nuclear"
-
-"""
-admm         ["L2", "L1", "L21", "TV", "Positive", "Proj"]
-cgnr         L2
-fista        ["L2", "L1", "L21", "TV", "Positive", "Proj"]
-optista      ["L2", "L1", "L21", "TV", "Positive", "Proj"] 
-pogm         ["L2", "L1", "L21", "TV", "Positive", "Proj"] 
-splitBregman ["L2", "L1", "L21", "TV", "Positive", "Proj"] 
-"""
-
-"""
-"kaczmarz"        - kaczmarz method (the default)
-"cgnr"            - CGNR
-"direct"          - A direct solver using the backslash operator
-"daxkaczmarz"     - Dax algorithm (with Kaczmarz) for unconstrained problems
-"daxconstrained"  - Dax algorithm for constrained problems
-"pseudoinverse"   - approximates a solution using the More-Penrose pseudo inverse
-"fusedlasso"      - solver for the Fused-Lasso problem
-"fista"           - Fast Iterative Shrinkage Thresholding Algorithm
-"optista"         - "Optimal" ISTA
-"pogm"            - Proximal Optimal Gradient Method
-"admm"            - Alternating Direcion of Multipliers Method
-"splitBregman"    - Split Bregman method for constrained & regularized inverse problems
-"primaldualsolver"- First order primal dual method
-"""
