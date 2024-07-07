17a183076973b8c4570d6dfdd6a204a2fb85e8f5
Benny <823573476@qq.com>
2024-07-07 19:26:09 +0800

    update

--- src/reconstruction/FFT.jl
+++
@@ -1,7 +0,0 @@
-
-
-
-convert_ifft(x;dims=[1,2]) = fftshift(ifft(ifftshift(x,dims),dims),dims)*prod(size(x)[dims])
-convert_fft(x;dims=[1,2])  = fftshift( fft(ifftshift(x,dims),dims),dims)/prod(size(x)[dims])
-
-
--- src/reconstruction/recon_2d.jl
+++ src/reconstruction/recon_2d.jl
@@ -1,4 +1,5 @@
-function reconstruct_2d_image(raw::RawAcquisitionData; Nx=nothing, Ny=nothing)
+function recon_2d(raw::RawAcquisitionData; Nx=nothing, Ny=nothing, outtype::Symbol=:mag, verbose=false)
+    @assert outtype in [:mag, :pha, :raw] "outtype must be :mag, :pha, :raw"
     acqData = AcquisitionData(raw)
     acqData.traj[1].circular = false #Removing circular window
     C = maximum(2*abs.(acqData.traj[1].nodes[:]))  #Normalize k-space to -.5 to .5 for NUFFT
@@ -10,25 +11,35 @@ function reconstruct_2d_image(raw::RawAcquisitionData; Nx=nothing, Ny=nothing)
     recParams[:reconSize] = (Nx, Ny)
     recParams[:densityWeighting] = true
     rec = reconstruction(acqData, recParams)
-    image3d  = reshape(rec.data, Nx, Ny, :)
-    image2d = (abs.(image3d) * prod(size(image3d)[1:2]))[:,:,1]
-    return image2d
-end
+    # nX, nY, nZ, nCon, nCoil, nRep = size(rec)
+    # size_info = Dict("nX"=>nX, "nY"=>nY, "nZ"=>nZ, "nCon"=>nCon, "nCoil"=>nCoil, "nRep"=>nRep) 
+    dims = ["nX", "nY", "nZ", "nCon", "nCoil", "nRep"]
 
-function reconstruct_2d_image_multi_coil(raw::RawAcquisitionData; Nx=nothing, Ny=nothing)
-    acqData = AcquisitionData(raw)
-    acqData.traj[1].circular = false #Removing circular window
-    C = maximum(2*abs.(acqData.traj[1].nodes[:]))  #Normalize k-space to -.5 to .5 for NUFFT
-    acqData.traj[1].nodes = acqData.traj[1].nodes[1:2,:] ./ C
-    if isnothing(Nx) || isnothing(Ny)
-        Nx, Ny = raw.params["reconSize"][1:2]
+    dims_squeezed = dims[findall(size(rec).>1)]
+    size_squeezed = size(rec)[findall(size(rec).>1)]
+
+    info_string = "\n  size = [ "
+    for idx in eachindex(dims_squeezed)
+        if idx != 1
+            info_string *= " | "
+        end
+        info_string *= "$(dims_squeezed[idx])=$(size_squeezed[idx])"
     end
-    recParams = Dict{Symbol,Any}()
-    recParams[:reconSize] = (Nx, Ny)
-    recParams[:densityWeighting] = true
-    rec = reconstruction(acqData, recParams)
-    nX, nY, nZ, nCon, nCoil, nRep = size(rec)
-    images = abs.(rec[:,:,1,1,:,1])
+    info_string *= " ]"
+    if verbose
+        @info "recon_2d: $(info_string)\n  type = $(String(outtype))"
+    end
+
+    if outtype == :mag
+        images = abs.(rec)
+    elseif outtype == :pha
+        images = angle.(rec)
+    else
+        images = rec
+    end
+
+    for dim in reverse(sort(findall(size(rec).==1)))
+        images = dropdims(images, dims=dim)
+    end
     return images
 end
-
