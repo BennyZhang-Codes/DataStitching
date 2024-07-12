e3eaa60d6075311b476f4cc1fc66e2a011e04732
Benny <823573476@qq.com>
2024-07-12 16:51:52 +0800

    update

--- example/paper/B0/B0_dephasing.jl
+++ example/paper/B0/B0_dephasing.jl
@@ -9,14 +9,14 @@ import ImageTransformations: imresize
 ############################################################################################## 
 simtype = SimType(B0=true, T2=false, ss=1)
 inplane_voxelsize = 0.05
-BHO = BlochHighOrder("000", true, true)
+BHO = BlochHighOrder("111", true, true)
 key = :fatsatwo  # :fatsatw, :fatsatwo
 
-dir = "$(@__DIR__)/B0/results/B0_dephassing/$(BHO.name)_$(inplane_voxelsize)_$(String(key))_admm_L1_50"; if ispath(dir) == false mkpath(dir) end
+dir = "$(@__DIR__)/B0/results/B0_dephassing/$(BHO.name)_$(inplane_voxelsize)_$(String(key))_admm_L1_0.001_50"; if ispath(dir) == false mkpath(dir) end
 maxOffresonance = 400.
 Nx = Ny = 150;
 
-for maxOffresonance in [400.]
+for maxOffresonance in [100., 200., 300., 400.]
 ############################################################################################## 
 # Simu
 ############################################################################################## 
@@ -70,9 +70,10 @@ tr_nominal = Trajectory(K_nominal_adc'[1:3,:], acqData.traj[1].numProfiles, acqD
 #######################################################################################
 # iterative HighOrderOp
 #######################################################################################
-for λ in [1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10]
+# for λ in [1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10]
+λ = 1e-3
 solver = "admm"
-iter = 10
+iter = 50
 reg = "L1"
 recParams = Dict{Symbol,Any}()
 recParams[:reconSize] = (Nx, Ny)  # 150, 150
@@ -87,35 +88,35 @@ recParams[:solver] = solver
 Op = HighOrderOp((Nx, Ny), tr_nominal, tr_skope, BHO; Nblocks=9, fieldmap=Matrix(B0map), grid=1)
 recParams[:encodingOps] = reshape([Op], 1,1)
 @time rec = reconstruction(acqData, recParams);
-p_iter_SignalOp = plot_image(abs.(rec.data[:,:]); title="HighOrderOp $(BHO.name), $(-maxOffresonance) Hz, iter $(iter), λ $(λ), $(solver), $(reg)", width=650, height=600)
-savefig(p_iter_SignalOp, dir*"/quadraticB0map_$(maxOffresonance)_reconHighOrderOp$(BHO.name)_iter$(iter)_lambda$(λ)_$(solver)_$(reg).svg", width=550,height=500,format="svg")
+p_iter_SignalOp = plot_image(abs.(rec.data[:,:]); title="HighOrderOp $(BHO.name), $(-maxOffresonance) Hz, $(solver)_$(reg)_iter$(iter)_λ$(λ)", width=650, height=600)
+savefig(p_iter_SignalOp, dir*"/quadraticB0map_$(maxOffresonance)_reconHighOrderOp$(BHO.name)_$(solver)_$(reg)_iter$(iter)_λ$(λ).svg", width=550,height=500,format="svg")
 end
 #######################################################################################
 # standard FieldmapNFFTOp
 #######################################################################################
-# nSample, nCoil = size(acqData.kdata[1])
-# dt = 1e-6
-# tAQ = (nSample-1) * dt
-# acqData.traj[1].AQ=tAQ # important for B0 correction
-# acqData.traj[1].TE= TE   # 0.0149415
-# acqData.traj[1].times = TE .+ collect(0:dt:tAQ)
+nSample, nCoil = size(acqData.kdata[1])
+dt = 1e-6
+tAQ = (nSample-1) * dt
+acqData.traj[1].AQ=tAQ # important for B0 correction
+acqData.traj[1].TE= TE   # 0.0149415
+acqData.traj[1].times = TE .+ collect(0:dt:tAQ)
 
-# acqData.traj[1].times = times;
-# recParams = Dict{Symbol,Any}()
-# recParams[:reconSize] = (Nx, Ny)  # 150, 150
-# recParams[:densityWeighting] = true
-# recParams[:reco] = "standard"
-# recParams[:method] = "nfft"
-# # recParams = merge(defaultRecoParams(), recParams)
+acqData.traj[1].times = times;
+recParams = Dict{Symbol,Any}()
+recParams[:reconSize] = (Nx, Ny)  # 150, 150
+recParams[:densityWeighting] = true
+recParams[:reco] = "standard"
+recParams[:method] = "nfft"
+# recParams = merge(defaultRecoParams(), recParams)
 
-# recParams[:regularization] = "L1" # ["L2", "L1", "L21", "TV", "LLR", "Positive", "Proj", "Nuclear"]
-# recParams[:λ] = 1e-3
-# recParams[:iterations] = 50
-# recParams[:solver] = "admm"
+recParams[:regularization] = reg # ["L2", "L1", "L21", "TV", "LLR", "Positive", "Proj", "Nuclear"]
+recParams[:λ] = λ
+recParams[:iterations] = iter
+recParams[:solver] = solver
 
-# recParams[:correctionMap] = 1im*B0map*2π;
-# @time rec = reconstruction(acqData, recParams);
-# p_iter_SignalOp = plot_image(abs.(rec.data[:,:]); title="FieldmapNFFTOp with B0map [-$maxOffresonance,$maxOffresonance] Hz", width=650, height=600)
-# savefig(p_iter_SignalOp, dir*"/quadraticB0map_$(maxOffresonance)_reconFieldmapNFFTOp.svg", width=550,height=500,format="svg")
+recParams[:correctionMap] = 1im*B0map*2π;
+@time rec = reconstruction(acqData, recParams);
+p_iter_SignalOp = plot_image(abs.(rec.data[:,:]); title="FieldmapNFFTOp, $(-maxOffresonance) Hz, $(solver)_$(reg)_iter$(iter)_λ$(λ)", width=650, height=600)
+savefig(p_iter_SignalOp, dir*"/quadraticB0map_$(maxOffresonance)_reconFieldmapNFFTOp$(BHO.name)_$(solver)_$(reg)_iter$(iter)_λ$(λ).svg", width=550,height=500,format="svg")
 end
 
