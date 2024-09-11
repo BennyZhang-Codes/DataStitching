90f1335cf7f16ca32f472f33ff74c7f77b1d082b
Benny <823573476@qq.com>
2024-09-11 14:33:24 +0800

    Fig5: add GroundTruth

---
+++ Figures/Fig_preset.jl
@@ -0,0 +1,38 @@
+using PyPlot, PyCall
+mpl_axes_grid1 = pyimport("mpl_toolkits.axes_grid1")
+
+mtransforms = PyPlot.plt.matplotlib.transforms
+mcolors = matplotlib.colors
+
+rcParams = PyPlot.PyDict(PyPlot.matplotlib."rcParams")
+rcParams["axes.titlepad"] = 2
+
+matplotlib.rc("mathtext", default="regular");
+matplotlib.rc("figure", dpi=200);
+matplotlib.rc("font", family="Times New Roman");
+matplotlib.rcParams["mathtext.default"];
+
+figure_width       = 5/2.54;
+figure_height      = 4/2.54;
+
+vmaxp              = 99;
+vminp              = 1;
+cmap               = "gray";
+
+fontsize_legend    = 5;
+fontsize_label     = 6;
+fontsize_subfigure = 8;
+fontsize_ticklabel = 4;
+
+linewidth          = 0.5;
+ticklength         = 1.5;
+
+pad_label          = 2;
+pad_labeltick      = 2;
+
+color_facecoler    = "#ffffff";
+color_label        = "#000000";
+color_subfigure    = "#ffffff";
+
+
+
