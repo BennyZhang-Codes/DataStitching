37e54a088235a0e28c8dd80e250ac2cc20229e09
Benny <823573476@qq.com>
2024-10-20 16:27:36 +0800

    Rename folder from example to workplace

--- example/debug/MRIReco.jl/MRIRecoBenchmarks/MRIRecoBenchmarks/runBenchmarks.jl
+++
@@ -1,8 +0,0 @@
-include("benchmark1/runBenchmark.jl")
-include("benchmark2/runBenchmark.jl")
-include("benchmark3/runBenchmark.jl")
-
-# just recreate the images
-#include("benchmark1/makeImages.jl")
-#include("benchmark2/makeImages.jl")
-#include("benchmark3/makeImages.jl")
