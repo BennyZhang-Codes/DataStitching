a2bb3ecd6ef06cc52fd9124a1ab1003174817a65
Benny <823573476@qq.com>
2024-10-27 20:32:02 +0800

    update

--- src/reconstruction/EncodingOperators/HighOrderOp.jl
+++ src/reconstruction/EncodingOperators/HighOrderOp.jl
@@ -78,6 +78,10 @@ function HighOrderOp(
         x, y = x .- Nx/2 .- 1, y .- Ny/2 .- 1
     elseif grid == 2
         x, y = x .- (Nx+1)/2, y .- (Ny+1)/2
+    elseif grid == 3
+        x, y = 1:Nx, 1:Ny
+        x, y, z = vec(x*0.0 .+ y'), vec(x[end:-1:1] .+ 0.0*y'), vec(x*0.0 .+ y'*0.0) #grid points
+        x, y = x .- (Nx+1)/2, y .- (Ny+1)/2
     end
     # print(x)
     x, y = x * Δx, y * Δy 
