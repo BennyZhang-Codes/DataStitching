897b58d7ffabc59f0f5926bf1b2923c48cf9d6e8
Benny <823573476@qq.com>
2024-10-27 16:05:47 +0800

    update

--- workplace/experiment/20241026/prep_data_syn.jl
+++ workplace/experiment/20241026/prep_data_syn.jl
@@ -21,15 +21,17 @@ gre_mrd_file = "$(path)/mrd/" * [f for f in readdir("$(path)/mrd") if occursin("
 
 if ispath("$(path)/syn") == false mkpath("$(path)/syn") end
 
-T              = Float32
+T = Float32
+
 kdata, ktraj, kdims, shape, TE, ReadoutMode, acqData, raw = read_gre(gre_seq_file, gre_mrd_file);
 nCha, nZ, nY, nX, nAvg, nSli, nCon, nPha, nRep, nSet, nSeg = shape;
 
 # IFFT of gre
-imgs = convert_ifft(kdata, dims=[2,3]);
-imgs = CoilCombineSOS(abs.(imgs), 1);
-imgs = permutedims(imgs, [3,1,2]);
-fig = plt_images(imgs,width=7.5, height=5, vminp=0, vmaxp=99)
+gre_imgs = convert_ifft(kdata, dims=[2,3]);
+gre_imgs = CoilCombineSOS(abs.(gre_imgs), 1);
+gre_imgs = permutedims(gre_imgs, [3,1,2]);
+fig = plt_images(gre_imgs,width=7.5, height=5, vminp=0, vmaxp=99)
+fig.savefig("$(path)/syn/$(basename(gre_mrd_file)[1:end-4])_sos.png", dpi=300, bbox_inches="tight", pad_inches=0)
 
 ############
 # espirit
@@ -37,6 +39,7 @@ fig = plt_images(imgs,width=7.5, height=5, vminp=0, vmaxp=99)
 sensitivity = espirit(acqData, (6,6), 30, eigThresh_1=0.02, eigThresh_2=0.99);  # (nX, nY, 1, nCha)
 csm = permutedims(sensitivity, [2,1,4,3])[:,:,:,1];# (nX, nY, 1, nCha) => (nY, nX, nCha, 1) => (nY, nX, nCha)
 fig = plt_images(permutedims(abs.(csm), [3,1,2]),width=10, height=5)  # (nCha, nY, nX)
+fig.savefig("$(path)/syn/$(basename(gre_mrd_file)[1:end-4])_CoilSens_espirit.png", dpi=300, bbox_inches="tight", pad_inches=0)
 
 ############
 # ΔB₀
@@ -49,16 +52,18 @@ images = convert_ifft(kdata[:,:,:,:], dims=[2,3]); # (nCha, nY, nX, nCon)
 ydata = permutedims(images, [2,3,1,4]);            # (nCha, nY, nX, nCon) => (nY, nX, nCha, nCon)
 
 b0, yik_sos = get_B0map(ydata, TE, smap, mask);
+fig = plt_images(permutedims(  abs.(yik_sos), [3,1,2]),width=7.5, height=5)
+fig = plt_images(permutedims(angle.(yik_sos), [3,1,2]),width=7.5, height=5)
+fig.savefig("$(path)/syn/$(basename(gre_mrd_file)[1:end-4])_pha.png"  , dpi=300, bbox_inches="tight", pad_inches=0)
 fig = plt_B0map(b0, width=5, height=4)
+fig.savefig("$(path)/syn/$(basename(gre_mrd_file)[1:end-4])_b0map.png", dpi=300, bbox_inches="tight", pad_inches=0, transparent=true)
 
-
-MAT.matwrite("$(path)/syn/syn_$(raw.params["protocolName"]).mat", Dict("csm" => csm, "b0" => b0, "mask" => mask))
-
+MAT.matwrite("$(path)/syn/syn_$(basename(gre_mrd_file)[1:end-4]).mat", Dict("csm" => csm, "b0" => b0, "mask" => mask))
 
 extract_data(r::Int64, path::String) = begin
     seq_file     = "$(path)/seq/" * [f for f in readdir("$(path)/seq") if occursin("r$(r)", f)][1]
     mrd_file     = "$(path)/mrd/" * [f for f in readdir("$(path)/mrd") if occursin("r$(r)", f)][1]
-
+    
     seq = read_seq(seq_file)[end-9:end-3]; 
     # times = KomaMRIBase.get_adc_sampling_times(seq);
     FOV        = seq.DEF["FOV"]
@@ -74,7 +79,7 @@ extract_data(r::Int64, path::String) = begin
 
     data = permutedims(reshape(kdata, nCha, nX*nSet), [2,1]);
 
-    MAT.matwrite("$(path)/syn/syn_$(raw.params["protocolName"]).mat", Dict("data" => data, "FOV" => FOV, "matrixSize" => matrixSize))
+    MAT.matwrite("$(path)/syn/syn_$(basename(mrd_file)[1:end-4]).mat", Dict("data" => data, "FOV" => FOV, "matrixSize" => matrixSize))
 end
 for r = 1:4
     extract_data(r, path)
