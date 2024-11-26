44380285b9170f614a6c86592088ebdfbace3fb7
Benny <823573476@qq.com>
2024-11-26 19:31:23 +0800

    update figures

--- Figures/Fig2/Fig2.jl
+++ Figures/Fig2/Fig2.jl
@@ -5,11 +5,11 @@ sh = SphericalHarmonics()
 nSegment_fully = 4
 nSegment_under = 36
 
-_, ntStitched_fully, _ = load_dfc(;dfc_method=:Stitched, seqname="demo", r=1);
-_, ntStitched_under, _ = load_dfc(;dfc_method=:Stitched, seqname="demo", r=30);
+_, ntStitched_fully, _ = load_dfc(;dfc_method=:Stitched, seqname="spiral", r=1);
+_, ntStitched_under, _ = load_dfc(;dfc_method=:Stitched, seqname="spiral", r=30);
 
-hoseq_fully = demo_hoseq(dfc_method=:Stitched)[4:end]   
-hoseq_under = demo_hoseq(dfc_method=:Stitched, r=30)[4:end]   
+hoseq_fully = load_hoseq(dfc_method=:Stitched)[4:end]   
+hoseq_under = load_hoseq(dfc_method=:Stitched, r=30)[4:end]   
 
 
 samples_fully = get_samples(hoseq_fully; off_val=Inf);
@@ -28,21 +28,39 @@ t_trigger_under = (s .+ 1)*dt .+ KomaMRIBase.get_block_start_times(hoseq_under.S
 
 matplotlib.rc("mathtext", default="regular")
 matplotlib.rc("figure", dpi=200)
-matplotlib.rc("font", family="Times New Roman")
+matplotlib.rc("font", family="Arial")
 matplotlib.rcParams["mathtext.default"]
-figure_width       = 18/2.54
-figure_height      = 5/2.54
-linewidth          = 0.5
+figure_width       = 17/2.54
+figure_height      = 4/2.54
+linewidth          = 0.8
+linewidth_marker   = 0.5
 ticklength         = 1.5
-fontsize_legend    = 5
-fontsize_label     = 6
-fontsize_ticklabel = 4
-fontsize_subfigure = 8
+fontsize_legend    = 8
+fontsize_label     = 8
+fontsize_ticklabel = 6
+fontsize_subfigure = 10
 pad_labeltick      = 2
 pad_label          = 2
+markersize_trigger = 40
+
 color_facecolor    = "#ffffff"
 color_label        = "#000000"
-color_trigger      = "#000000"
+color_trigger      = "#555555"
+
+color_gx           = "#636EFA"
+color_gy           = "#EF553B"
+color_gz           = "#00CC96"
+color_rf           = "#AB63FA"
+
+color_gx           = "#6699CC"
+color_gy           = "#FF6666"
+color_gz           = "#669966"
+color_rf           = "#CC66CC"
+
+# color_gx           = "C0"
+# color_gy           = "C3"
+# color_gz           = "C2"
+# color_rf           = "C5"
 
 fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(figure_width, figure_height), facecolor=color_facecolor)
 
@@ -51,30 +69,31 @@ t_trigger_list = [t_trigger_fully, t_trigger_under]
 t_adc_list = [t_adc_fully, t_adc_under]
 
 
-
 for (ax, samples, t_trigger) in zip(axs, samples_list, t_trigger_list)
     ax.tick_params(axis="both", length=ticklength, width=linewidth, color=color_label, labelcolor=color_label, labelsize=fontsize_ticklabel)
     for spine in ax.spines  # "left", "right", "bottom", "top"
         ax.spines[spine].set_color(color_label)
         ax.spines[spine].set_visible(false)
     end
-    ax.set_xlim(samples.gz.t[1]*1e3, samples.gz.t[end]*1e3)
+    ax.set_xlim(samples.gz.t[1]*1e3 - 1, samples.gz.t[end]*1e3+1)
     ax.set_facecolor(color_facecolor)
     ax.yaxis.set_major_locator(plt.MultipleLocator(20))
 
     ax.axhline(y=0, color="#555555", linewidth=linewidth)
-    ax.plot(samples.gx.t*1e3,       samples.gx.A*1e3, color="#636EFA", linewidth=linewidth, label=L"G_{x}")
-    ax.plot(samples.gy.t*1e3,       samples.gy.A*1e3, color="#EF553B", linewidth=linewidth, label=L"G_{y}")
-    ax.plot(samples.gz.t*1e3,       samples.gz.A*1e3, color="#00CC96", linewidth=linewidth, label=L"G_{z}")
+    ax.plot(samples.gx.t*1e3,       samples.gx.A*1e3, color=color_gx, linewidth=linewidth, label=L"G_{x}")
+    ax.plot(samples.gy.t*1e3,       samples.gy.A*1e3, color=color_gy, linewidth=linewidth, label=L"G_{y}")
+    ax.plot(samples.gz.t*1e3,       samples.gz.A*1e3, color=color_gz, linewidth=linewidth, label=L"G_{z}")
 
-    ax.plot(samples.rf.t*1e3, abs.(samples.rf.A)*1e7, color="#AB63FA", linewidth=linewidth, label=L"|B_{1}|")
+    ax.plot(samples.rf.t*1e3, abs.(samples.rf.A)*1e7, color=color_rf, linewidth=linewidth, label=L"|B_{1}|")
 
-    ax.scatter(t_trigger*1e3, -60*ones(length(t_trigger)), s=15, marker=L"\mapsup", color=color_trigger, linewidth=0.1, label=L"trigger")
+    ax.scatter(t_trigger*1e3, -60*ones(length(t_trigger)), s=markersize_trigger, marker=L"\uparrow", color=color_trigger, linewidth=linewidth_marker, label=L"Trigger")
 
     ax.set_ylim(-75, 75)
-    ax.set_ylabel("amplitude (mT/m, mG)", fontsize=fontsize_label, color=color_label)
-    ax.set_xlabel("time (ms)", fontsize=fontsize_label, color=color_label)
-    ax.legend(fontsize=fontsize_legend, labelcolor=color_label, ncols=5, loc="upper left", frameon=false, handlelength=1, handletextpad=0.5, columnspacing=1)
+    ax.set_ylabel("Amplitude [mT/m, mG]", fontsize=fontsize_label, color=color_label)
+    ax.set_xlabel("Time [ms]", fontsize=fontsize_label, color=color_label)
+    ax.legend(bbox_to_anchor=(0.01, 1.1), fontsize=fontsize_legend, labelcolor=color_label, 
+        scatteryoffsets=[0.5],
+        ncols=5, loc="upper left", frameon=false, handlelength=1, handletextpad=0.5, columnspacing=1)
 end
 
 fig.align_ylabels()
@@ -82,7 +101,8 @@ orders = ["a", "b"]
 
 for col = 1:2
     order = orders[col]
-    fig.text(0.01+(col-1)/2, 1, "($(order))", ha="left", va="baseline", fontsize=fontsize_subfigure, color=color_label)
+    fig.text(0+(col-1)/2, 1, "($(order))", ha="left", va="baseline", fontsize=fontsize_subfigure, color=color_label)
 end
-fig.tight_layout(pad=0, h_pad=0.5, w_pad=0.5)
-fig.savefig("Figures/Fig2/Fig2.png", dpi=300, bbox_inches="tight")
+fig.tight_layout(pad=0, h_pad=0.5, w_pad=0.8)
+fig.savefig("Figures/Fig2/Fig2.png", dpi=900, transparent=false, bbox_inches="tight", pad_inches=0.05)
+fig.savefig("Figures/Fig2/Fig2.svg", dpi=900, transparent=false, bbox_inches="tight", pad_inches=0.05)
--- Figures/Fig3/Fig3.jl
+++ Figures/Fig3/Fig3.jl
@@ -1,19 +1,25 @@
 using PyPlot
 using KomaHighOrder
 using LinearAlgebra
+legend_handler = PyPlot.matplotlib.legend_handler
+collections = PyPlot.matplotlib.collections
+lines = PyPlot.matplotlib.lines
+Patch = matplotlib.patches.Patch
+
+outpath = "$(@__DIR__)/Figures/Fig3/out"; if ispath(outpath) == false mkpath(outpath) end     # output directory
 
 sh = SphericalHarmonics()
 nSegment_fully = 4
 nSegment_under = 36
 
-_, ntStitched_fully, _ = load_dfc(;dfc_method=:Stitched, seqname="demo", r=1)
-_, ntStitched_under, _ = load_dfc(;dfc_method=:Stitched, seqname="demo", r=30)
+_, ntStitched_fully, _ = load_dfc(;dfc_method=:Stitched, seqname="spiral", r=1)
+_, ntStitched_under, _ = load_dfc(;dfc_method=:Stitched, seqname="spiral", r=30)
 
 # seq
-hoseq_fully_Standard = demo_hoseq(dfc_method=:Standard)[8]   # :Standard or :Stitched
-hoseq_fully_Stitched = demo_hoseq(dfc_method=:Stitched)[8]   # :Standard or :Stitched
-hoseq_under_Standard = demo_hoseq(dfc_method=:Standard, r=30)[8]   # :Standard or :Stitched
-hoseq_under_Stitched = demo_hoseq(dfc_method=:Stitched, r=30)[8]   # :Standard or :Stitched
+hoseq_fully_Standard = load_hoseq(dfc_method=:Standard)[8]   # :Standard or :Stitched
+hoseq_fully_Stitched = load_hoseq(dfc_method=:Stitched)[8]   # :Standard or :Stitched
+hoseq_under_Standard = load_hoseq(dfc_method=:Standard, r=30)[8]   # :Standard or :Stitched
+hoseq_under_Stitched = load_hoseq(dfc_method=:Stitched, r=30)[8]   # :Standard or :Stitched
 
 # gradient
 samples_fully_Standard = get_samples(hoseq_fully_Standard; off_val=0); 
@@ -42,36 +48,43 @@ _, _, k_dfc_under_Stitched, k_dfc_adc_under_Stitched = get_kspace(hoseq_under_St
 # end
 
 a = Int64.(vec(ntStitched_fully))
-a[1] += 3
+a[1] += 2
 e = cumsum(a)
 s = e .- a
 Segment_range_fully = diag([st+1:en+1 for st in s, en in e])
 
 
 a = Int64.(vec(ntStitched_under))
-a[1] += 3
+a[1] += 2
 e = cumsum(a)
 s = e .- a
 Segment_range_under = diag([st+1:en+1 for st in s, en in e])
 
 matplotlib.rc("mathtext", default="regular")
 matplotlib.rc("figure", dpi=200)
-matplotlib.rc("font", family="Times New Roman")
+matplotlib.rc("font", family="Arial")
 matplotlib.rcParams["mathtext.default"]
-figure_width       = 18/2.54
-figure_height      = 11/2.54
-linewidth          = 0.5
+figure_width       = 17/2.54
+figure_height      = 10/2.54
+linewidth          = 0.8
 ticklength         = 1.5
-fontsize_legend    = 5
-fontsize_label     = 6
-fontsize_ticklabel = 4
-fontsize_subfigure = 8
+fontsize_legend    = 7
+fontsize_label     = 7
+fontsize_ticklabel = 6
+fontsize_subfigure = 10
 pad_labeltick      = 2
 pad_label          = 2
 color_facecolor    = "#ffffff"
 color_label        = "#000000"
 color_difference   = "#000000"
 color_segment      = ["#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd",  "#e377c2",  "#bcbd22","#17becf"]#,"#8c564b"  ,"#7f7f7f"
+color_gx           = "C0"
+color_gy           = "C1"
+
+# color_segment      = ["#6699CC","#FF6666","#669966","#CC66CC","#FF9900"]#,"#8c564b"  ,"#7f7f7f"
+# color_gx           = "#6699CC"
+# color_gy           = "#FF6666"
+
 fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(figure_width, figure_height), 
                         facecolor=color_facecolor)
 ax1, ax2, ax3, ax4, ax5, ax6 = axs # unpack the 3 axes for zeroth-order, first-order, and second-order plots
@@ -92,27 +105,27 @@ for ax in axs
 end
 
 titles = [L"G_x", L"G_y", "k-space trajectory"]
-xlabels = ["Time (ms)", "Time (ms)", L"k_x (m⁻¹)"]
-ylabels = ["Amplitude (mT/m)", "Amplitude (mT/m)", L"k_y (m⁻¹)"]
+xlabels = ["Time [ms]", "Time [ms]", L"k_x \ [rad/m]"]
+ylabels = [L"G_x \ [mT/m]", L"G_x \ [mT/m]", L"k_y \ [rad/m]"]
 for row = 1:2
     for col = 1:3
         ax = axs[row,col]
         title = titles[col]
         xlabel = xlabels[col]
         ylabel = ylabels[col]
-        ax.set_title(title, fontsize=fontsize_label, color=color_label)
+        # ax.set_title(title, fontsize=fontsize_label, color=color_label, pad=6)
         ax.set_xlabel(xlabel, fontsize=fontsize_label, color=color_label, labelpad=pad_label)
         ax.set_ylabel(ylabel, fontsize=fontsize_label, color=color_label, labelpad=pad_label)
     end
 end
 
 for ax in axs[1, 1:2] 
-    ax.set_xlim(t_adc_fully[1], t_adc_fully[end])
+    ax.set_xlim(t_adc_fully[1]-1, t_adc_fully[end]+1)
     ax.set_ylim(-vmax_g_fully, vmax_g_fully)
     ax.yaxis.set_major_locator(plt.MultipleLocator(round(vmax_g_fully/4, sigdigits=1)))
 end
 for ax in axs[2, 1:2] 
-    ax.set_xlim(t_adc_under[1], t_adc_under[end])
+    ax.set_xlim(t_adc_under[1]-1, t_adc_under[end]+1)
     ax.set_ylim(-vmax_g_under, vmax_g_under)
     ax.yaxis.set_major_locator(plt.MultipleLocator(round(vmax_g_under/4, sigdigits=1)))
 end
@@ -120,13 +133,13 @@ end
 # fully
 for seg = 1:nSegment_fully
     seg_r = Segment_range_fully[seg]
-    axs[1,1].plot(t_adc_fully[seg_r], samples_fully_Stitched.h1.A[seg_r]*1e3, color=color_segment[seg], linewidth=linewidth, label="Stitched segment $(seg)")
-    axs[1,2].plot(t_adc_fully[seg_r], samples_fully_Stitched.h2.A[seg_r]*1e3, color=color_segment[seg], linewidth=linewidth, label="Stitched segment $(seg)")
+    axs[1,1].plot(t_adc_fully[seg_r], samples_fully_Stitched.h1.A[seg_r]*1e3, color=color_segment[seg], linewidth=linewidth, label="Segment $(seg)")
+    axs[1,2].plot(t_adc_fully[seg_r], samples_fully_Stitched.h2.A[seg_r]*1e3, color=color_segment[seg], linewidth=linewidth, label="Segment $(seg)")
 end
 axs[1,1].plot(t_adc_fully, (samples_fully_Stitched.h1.A-samples_fully_Standard.h1.A)*1e3, color=color_difference, linewidth=linewidth, label="Difference")
 axs[1,2].plot(t_adc_fully, (samples_fully_Stitched.h2.A-samples_fully_Standard.h2.A)*1e3, color=color_difference, linewidth=linewidth, label="Difference")
-axs[1,3].plot(k_dfc_adc_fully_Stitched[:,2], k_dfc_adc_fully_Stitched[:,3], color="C1", linewidth=linewidth, label="Stitched measurement")
-axs[1,3].plot(k_dfc_adc_fully_Standard[:,2], k_dfc_adc_fully_Standard[:,3], color="C0", linewidth=linewidth, label="Single measurement")
+axs[1,3].plot(k_dfc_adc_fully_Stitched[:,2]*2π, k_dfc_adc_fully_Stitched[:,3]*2π, color=color_gx, linewidth=linewidth, label="Stitched")
+axs[1,3].plot(k_dfc_adc_fully_Standard[:,2]*2π, k_dfc_adc_fully_Standard[:,3]*2π, color=color_gy, linewidth=linewidth, label="Standard")
 axs[1,3].set_aspect(1)
 
 # under
@@ -135,33 +148,46 @@ for seg = 1:nSegment_under
     color_idx = seg%nColor != 0 ? seg%nColor : nColor
     
     seg_r = Segment_range_under[seg]
-    axs[2,1].plot(t_adc_under[seg_r], samples_under_Stitched.h1.A[seg_r]*1e3, color=color_segment[color_idx], linewidth=linewidth, label="Stitched measurement")
-    axs[2,2].plot(t_adc_under[seg_r], samples_under_Stitched.h2.A[seg_r]*1e3, color=color_segment[color_idx], linewidth=linewidth, label="Stitched measurement")
+    axs[2,1].plot(t_adc_under[seg_r], samples_under_Stitched.h1.A[seg_r]*1e3, color=color_segment[color_idx], linewidth=linewidth, label="Stitched")
+    axs[2,2].plot(t_adc_under[seg_r], samples_under_Stitched.h2.A[seg_r]*1e3, color=color_segment[color_idx], linewidth=linewidth, label="Stitched")
 end 
 lDiff_gx, = axs[2,1].plot(t_adc_under, (samples_under_Stitched.h1.A-samples_under_Standard.h1.A)*1e3, color=color_difference, linewidth=linewidth, label="Difference")
 lDiff_gy, = axs[2,2].plot(t_adc_under, (samples_under_Stitched.h2.A-samples_under_Standard.h2.A)*1e3, color=color_difference, linewidth=linewidth, label="Difference")
-axs[2,3].plot(k_dfc_adc_under_Stitched[:,2], k_dfc_adc_under_Stitched[:,3], color="C1", linewidth=linewidth, label="Stitched measurement")
-axs[2,3].plot(k_dfc_adc_under_Standard[:,2], k_dfc_adc_under_Standard[:,3], color="C0", linewidth=linewidth, label="Single measurement")
+axs[2,3].plot(k_dfc_adc_under_Stitched[:,2]*2π, k_dfc_adc_under_Stitched[:,3]*2π, color=color_gx, linewidth=linewidth, label="Stitched")
+axs[2,3].plot(k_dfc_adc_under_Standard[:,2]*2π, k_dfc_adc_under_Standard[:,3]*2π, color=color_gy, linewidth=linewidth, label="Standard")
 axs[2,3].set_aspect(1)
 
 
 # legend
-for (ax, ncol) in zip(axs[[1,3, 5, 6]], [2, 2, 2, 2])   # setup legend for each subplot
+for (ax, ncol) in zip(axs[[5,6]], [2,2])   # setup legend for each subplot
     ax.legend(fontsize=fontsize_legend, labelcolor=color_label, ncols=ncol, 
-    loc="upper left", 
-    bbox_to_anchor=(0,1.05),
+    loc="center left", bbox_to_anchor=(0,1.0),
     frameon=false, handlelength=1, handletextpad=0.5, columnspacing=1,labelspacing=0.2)
 end
-ls = []
-for color in color_segment
-    push!(ls, lines.Line2D([], [], color=color, linewidth=linewidth))
-end
-for (ax, lDiff) in zip(axs[2,1:2], [lDiff_gx, lDiff_gy])
-    ax.legend([Tuple(ls), lDiff], ["Stitched measurement", "Difference"] , handler_map=Dict(Tuple(ls) => legend_handler.HandlerTuple(ndivide=length(color_segment), pad=0)),
-        fontsize=fontsize_legend, labelcolor=color_label, ncols=2, loc="upper left", bbox_to_anchor=(0,1.05),
-        frameon=false, handlelength=0.4*length(color_segment), handletextpad=0.5, columnspacing=1,labelspacing=0.2)
+
+for (ax, ncol) in zip(axs[[1,3]], [2,2])   # setup legend for each subplot
+    ls = []
+    for color in color_segment[1:4]
+        push!(ls, lines.Line2D([], [], color=color, linewidth=linewidth*2))
+    end
+    ax.legend([Tuple(ls), lines.Line2D([], [], color=color_difference, linewidth=linewidth*2)], ["Stitched", "Difference"], 
+        handler_map=Dict(Tuple(ls) => legend_handler.HandlerTuple(ndivide=4, pad=0)),
+        fontsize=fontsize_legend, labelcolor=color_label, ncols=2, 
+        loc="center left", bbox_to_anchor=(0,1.0),
+        frameon=false, handlelength=2.5, handletextpad=0.5, columnspacing=1,labelspacing=0.2)
 end
 
+ls = []
+for color in color_segment
+    push!(ls, lines.Line2D([], [], color=color, linewidth=linewidth*2))
+end
+for (ax, lDiff) in zip(axs[2,1:2], [lDiff_gx, lDiff_gy])
+    ax.legend([Tuple(ls), lines.Line2D([], [], color=color_difference, linewidth=linewidth*2)], ["Stitched", "Difference"], 
+        handler_map=Dict(Tuple(ls) => legend_handler.HandlerTuple(ndivide=length(color_segment), pad=0)),
+        fontsize=fontsize_legend, labelcolor=color_label, ncols=2, 
+        loc="center left", bbox_to_anchor=(0,1.0),
+        frameon=false, handlelength=2.5, handletextpad=0.5, columnspacing=1,labelspacing=0.2)
+end
 
 
 fig.align_ylabels()
@@ -172,5 +198,6 @@ for row = 1:2
         fig.text(0.01+(col-1)/3, 1-0.5*(row-1), "($(order))", ha="left", va="baseline", fontsize=fontsize_subfigure, color=color_label)
     end
 end
-fig.tight_layout(pad=0, h_pad=0.5, w_pad=0.5)
-fig.savefig("Figures/Fig3/Fig3.png", dpi=300, bbox_inches="tight")
+
+fig.tight_layout(pad=0, h_pad=0.5, w_pad=0.1)
+fig.savefig("$(outpath)/Fig3.png", dpi=900, transparent=false, bbox_inches="tight", pad_inches=0.05)
